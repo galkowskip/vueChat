28e93afc271a892c8684795a86f902245ce0506f
galap <galkowski.p97@gmail.com>
2023-01-18 19:21:06 +0100

    get messages

--- src/components/ChatContainer.vue
+++ src/components/ChatContainer.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <div></div>
+    <div class="list">
+      <chat-message
+        v-for="(item, index) in data"
+        :item="item"
+        :key="`${index}-${item.user.uid}`"
+        :continousMessage="index > 0 ? data[index - 1].user.uid === item.user.uid : false"
+        :isActiveUsers="item.user.uid === user.uid"
+      />
+    </div>
     <div>
       <input type="text" v-model="newMessage" />
       <button @click="sendMessage">Send</button>
@@ -9,28 +17,62 @@
 </template>
 
 <script>
-import { ref, set } from "firebase/database";
+import { ref, set, onValue } from "firebase/database";
 import { db } from "../firebase";
 
+import ChatMessage from "./ChatMessage.vue";
+
 export default {
+  components: { ChatMessage },
   props: {
     user: Object,
   },
   data: function () {
     return {
       newMessage: "",
+    //   messagesRef: ref(db, "messages/"),
+      data: [],
     };
+  },
+  computed: {
+      messagesRef: function() {
+          return  ref(db, `messages/${Date.now()}`)
+      }
+  },
+  mounted: function () {
+    onValue(this.messagesRef, (snapshot) => {
+      const data = snapshot.val();
+      if (!data) {
+        return;
+      }
+
+      if (typeof data === "object" && Object.keys(data).length) {
+        this.data = [...this.data, data];
+      } else {
+        this.data = [...this.data, ...data];
+      }
+    });
   },
   methods: {
     sendMessage: function () {
-      set(ref(db, "messages/" + Date.now()), {
-        user: this.user.displayName,
+      set(this.messagesRef, {
+        user: {
+          name: this.user.displayName,
+          uid: this.user.uid,
+          email: this.user.email,
+        },
         message: this.newMessage,
+        timestamp: Date.now(),
       });
     },
   },
 };
 </script>
 
-<style>
+<style scoped>
+.list {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+}
 </style>
---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="message-item"
+    :class="isActiveUsers ? 'message-item--yours' : 'message-item--someones'"
+  >
+    <div class="message-item__title" v-if="!continousMessage">{{ item.user.name }}</div>
+    <div class="message-item__content">{{ item.message }}</div>
+    <div class="message-item__time-ago">{{ timeAgo }}</div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  props: {
+    item: Object,
+    isActiveUsers: Boolean,
+    continousMessage: Boolean
+  },
+  computed: {
+    timeAgo: function () {
+      return moment(this.item.timestamp).toNow();
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.message-item {
+  align-self: flex-start;
+  display: flex;
+  flex-direction: column;
+  padding: 12px 24px;
+  &--yours {
+    align-self: flex-end;
+  }
+}
+</style>
